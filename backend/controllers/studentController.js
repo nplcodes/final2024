adffd316661fc7736b673acdff4bed7985d57667
amin-leon <145145606+nplcodes@users.noreply.github.com>
2024-01-22 21:44:49 +0200

    staff and student actions ...ends ....

--- backend/controllers/studentController.js
+++ backend/controllers/studentController.js
@@ -12,4 +12,46 @@ const registerStudent = async (req, res) => {
   }
 };
 
-export default { registerStudent };
+const updateStudent = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const updatedStudent = await Student.findByIdAndUpdate(id, req.body, { new: true });
+    if (!updatedStudent) {
+      return res.status(404).json({ message: "Student not found" });
+    }
+
+    res.status(200).json({ message: "Student data updated successfully", student: updatedStudent });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+const deleteStudent = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const deletedStudent = await Student.findByIdAndDelete(id);
+    if (!deletedStudent) {
+      return res.status(404).json({ message: "Student not found" });
+    }
+
+    res.status(200).json({ message: "Student deleted successfully", student: deletedStudent });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+const getAllStudents = async (req, res) => {
+    try {
+      const allStudents = await Student.find();
+      res.status(200).json({ students: allStudents });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: "Internal Server Error" });
+    }
+  };
+
+export default { registerStudent, updateStudent, deleteStudent, getAllStudents  };
--- backend/routes/studentRoutes.js
+++ backend/routes/studentRoutes.js
@@ -1,3 +1,4 @@
+
 // studentRoutes.js
 import express from "express";
 import studentController from "../controllers/studentController.js";
@@ -5,5 +6,10 @@ import studentController from "../controllers/studentController.js";
 const router = express.Router();
 
 router.post("/student/register", studentController.registerStudent);
+router.put("/student/update/:id", studentController.updateStudent);
+router.delete("/student/delete/:id", studentController.deleteStudent);
+router.get("/student/all", studentController.getAllStudents);
+
 
 export default router;
+
